fdd90812414d11367bf0b4bbefbb7aabdd52c7de
lieby weintraub <liebahinda@gmail.com>
2019-02-18 18:44:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,10 @@ def reverse_each_word(sentence)
   sentence.join(" ")
 end 
 
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+    word.reverse_each_word
+  end 
+  sentence.join(" ")
+end
+end 
